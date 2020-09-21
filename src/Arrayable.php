6aec9264038ca34fa29a368981fd0d221a835c94
Andrey <inbox.shelamkoff@gmail.com>
2020-09-21 21:14:56 +0300

    Create Arrayable.php

---
+++ src/Arrayable.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace Bermuda;
+
+
+/**
+ * Interface Arrayable
+ * @package Bermuda
+ */
+interface Arrayable
+{
+    /**
+     * @return array
+     */
+    public function toArray(): array ;
+}
