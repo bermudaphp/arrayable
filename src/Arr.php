549ac0d2529f0cb8c63a60ab101e6caa92470e87
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-11-25 17:22:26 +0200

    Update Arr.php

--- src/Arr.php
+++ src/Arr.php
@@ -2,138 +2,115 @@
 
 namespace Bermuda;
 
-/**
- * @param $var
- * @return array
- */
-function to_array($var): array
+final class Arr
 {
-    if (is_array($var)) {
-        return $var;
+    /**
+     * @param $var
+     * @return array
+     */
+    public static function toArray($var): array
+    {
+        return to_array($var);
     }
 
-    if ($var instanceof Arrayable) {
-        return $var->toArray();
+    /**
+     * @param array|\ArrayAccess $accessible
+     * @param $offset
+     * @param null $default
+     * @return mixed|null
+     */
+    public static function pull(array|\ArrayAccess &$accessible, $offset, $default = null)
+    {
+        $value = $array[$offset] ?? $default;
+        unset($array[$offset]);
+        
+        return $value;
     }
-
-    if (is_iterable($var)) {
-        return iterator_to_array($var);
-    }
-
-    return [$var];
-}
-
-/**
- * @param array|\ArrayAccess $accessible
- * @param string|int $offset
- * @param null $default
- * @return mixed
- */
-function array_pull(array|\ArrayAccess &$accessible, string|int $offset, $default = null): mixed
-{
-    $value = $array[$offset] ?? $default;
-    unset($array[$offset]);
-
-    return $value;
-}
-
-/**
- * @param array|\ArrayAccess $accessible
- * @param string[]|int[] $offsets
- * @param null $default
- * @return array
- */
-function array_pull_all(array|\ArrayAccess &$accessible, array $offsets, $default = null): array
-{
-    $vars = [];
-    foreach($offsets as $offset) {
-        $vars[$offset] = array_pull($array, $offset, is_array($default) ? $default[$offset] ?? null : $default);
-    }
-
-    return $vars;
-}
-
-/**
- * @param array|\ArrayAccess $accessible
- * @param int|string $key
- * @return bool
- */
-function array_key_exists(array|\ArrayAccess $accessible, int|string $key): bool
-{
-    return $accessible instanceof \ArrayAccess ? $accessible->offsetExists($key) : \array_key_exists($key, $accessible);
-}
-
-/**
- * @param array|\ArrayAccess $accessible
- * @param string|string[] $offsets
- * @return array
- */
-function array_only(array|\ArrayAccess $accessible, string|array $offsets): array
-{
-    foreach (to_array($offsets) as $offset) {
-        if (array_key_exists($offset, $array)) {
-            $only[$offset] = $array[$offset];
+    
+    /**
+     * @param array|\ArrayAccess $accessible
+     * @param string[]|int[] $offsets
+     * @param null $default
+     * @return array|\ArrayAccess
+     */
+    public static function pullAll(array|\ArrayAccess &$accessible, array $offsets, $default = null): array|\ArrayAccess
+    {
+        $vars = [];
+        foreach($offsets as $offset) {
+            $vars[$offset] = self::pull($array, $offset, is_array($default) ? $default[$offset] ?? null : $default);
         }
+        
+        return $vars;
+    } 
+
+    /**
+     * @param array|ArrayAccess $accessible
+     * @param string|int|int[]|string[] $offsets
+     * @return array|\ArrayAccess
+     */
+    public static function only(array|\ArrayAccess $accessible, string|int|array $offsets): array|\ArrayAccess
+    {
+        foreach (to_array($offsets) as $offset) {
+            if (array_key_exists($offset, $array)) {
+                $only[$offset] = $array[$offset];
+            }
+        }
+        
+        return $only ?? [];
     }
 
-    return $only ?? [];
-}
+    /**
+     * @param array|\ArrayAccess $accessible
+     * @param string|string[] $offsets
+     * @return array|\ArrayAccess
+     */
+    public static function remove(array|\ArrayAccess &$accessible, string|array $offsets): array|\ArrayAccess
+    {
+        foreach (to_array($offsets) as $offset) {
+            unset($array[$offset]);
+        }
 
-/**
- * @param array|\ArrayAccess $accessible
- * @param string|string[] $offsets
- * @return array|\ArrayAccess
- */
-function array_remove(array|\ArrayAccess &$accessible, string|array $offsets): array|\ArrayAccess
-{
-    foreach (to_array($offsets) as $offset) {
-        unset($accessible[$offset]);
+        return $array;
     }
 
-    return $accessible;
-}
-
-/**
- * @param string $subject
- * @param string $separator
- * @param int|null $limit
- * @return array
- */
-function explode(string $subject, string $separator = ',', ?int $limit = null): array
-{
-    return explode($separator, $subject, $limit);
-}
-
-/**
- * @param array $array
- * @param callable $callback
- * @return array
- */
-function array_map(array &$array, callable $callback): array
-{
-    return $array = \array_map($callback, $array);
-}
-
-/**
- * @param array $array
- * @param callable $callback
- * @return array
- */
-function array_filter(array &$array, callable $callback): array
-{
-    return $array = \array_filter($array, $callback, ARRAY_FILTER_USE_BOTH);
-}
-
-/**
- * @param array|\ArrayAccess $accessible
- * @param string|string[] $offsets
- * @return array|\ArrayAccess
- */
-function array_except(array|\ArrayAccess $accessible, string|array $offsets): array|\ArrayAccess
-{
-    if ($accessible instanceof \ArrayAccess) {
-        $accessible = clone $accessible;
+    /**
+     * @param string $subject
+     * @param string $separator
+     * @param int|null $limit
+     * @return array
+     */
+    public static function explode(string $subject, string $separator = ',', ?int $limit = null): array
+    {
+        return explode($separator, $subject, $limit);
     }
 
-    return array_remove($accessible, $offsets);
+    /**
+     * @param array $array
+     * @param callable $callback
+     * @return array
+     */
+    public static function map(array &$array, callable $callback): array
+    {
+        return $array = array_map($callback, $array);
+    }
+
+    /**
+     * @param array $array
+     * @param callable $callback
+     * @return array
+     */
+    public static function filter(array &$array, callable $callback): array
+    {
+        return $array = array_filter($array, $callback, ARRAY_FILTER_USE_BOTH);
+    }
+
+    /**
+     * @param array|\ArrayAccess $accessible
+     * @param string|int|string[]|int[] $offsets
+     * @return array|ArrayAccess
+     */
+    public static function except(array|\ArrayAccess $accessible, string|int|array $offsets): array|ArrayAccess
+    {
+        return self::remove($array, $offsets);
+    }
 }
